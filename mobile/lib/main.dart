90b54b3ae15b10cc81bf4200cfeb2cb8a647f038
darijavan <darijavan@gmail.com>
2020-01-26 17:04:21 +0300

    add change notifier

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:mobile/src/app.dart';
+import 'package:mobile/src/provider/user.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (BuildContext context) => UserModel(),
+      child: MyApp(),
+    ),
+  );
 
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 }
