1686be2138b6d4051b436254af43b011b9910af4
darijavan <darijavan@gmail.com>
2020-04-04 11:14:13 +0300

    set basic messaging config for socket

--- server/config/socket.js
+++ server/config/socket.js
@@ -1,10 +1,29 @@
+var OptimizedMap = require('../classes/Map');
+
 /**
  * Initialize SocketIO server
  * @param {SocketIO.Server} io The socket server
  */
 function initSocket(io) {
+  const currentConnections = new OptimizedMap();
+
   io.on('connection', function (socket) {
-    // TODO
+    currentConnections.set(socket, {});
+
+    socket.emit('user-connected', currentConnections.size);
+
+    socket.on('user-data', function (data) {
+      currentConnections.set(socket, data);
+    });
+
+    socket.on('send-message', function (data) {
+      if (typeof data.targetId === 'string')
+        socket.broadcast.to(data.targetId).emit('message', data.message);
+    });
+
+    socket.on('disconnect', function () {
+      currentConnections.delete(socket);
+    });
   });
 }
 
