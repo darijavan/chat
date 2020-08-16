e0d5237ead57feaf6b92b52c85387b3d9a1ff5f3
RANDRIANANDRAINA Hajanirina Ridjvan <darijavan@gmail.com>
2020-08-16 12:19:51 +0300

    Update users.js

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,10 +2,53 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 
+var hash = require('../helpers/hash');
+var User = require('../models/user');
+
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   res.send({
     message: 'respond with a resource'
+  });
+});
+
+router.post('/logout', function (req, res, next) {
+  req.logOut();
+  res.redirect('/');
+});
+
+router.post('/signup', function (req, res, next) {
+  var username = req.body.username,
+    password = req.body.password;
+
+  User.findOne({
+    username: username,
+  }, function (err, user) {
+    if (err) return res.sendStatus(500);
+
+    if (user) return res.json({
+      status: 'failed',
+      message: 'Username already in use'
+    });
+
+    var newUser = new User({
+      username: username,
+      password: hash.hash(password),
+    });
+
+    newUser.save(function (err, newUser) {
+      if (err) return res.sendStatus(500);
+
+      req.logIn(newUser, function (err) {
+        if (err) throw err;
+
+        return res.json({
+          status: 'success',
+          user: newUser,
+          sessionToken: req.sessionID,
+        });
+      });
+    });
   });
 });
 
