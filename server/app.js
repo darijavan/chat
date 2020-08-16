a1516e2261cde981f838e286d80a86ff8b1c93d0
RANDRIANANDRAINA Hajanirina Ridjvan <darijavan@gmail.com>
2020-08-16 12:19:30 +0300

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,17 +1,18 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var session = require('express-session');
-var mongoose = require('mongoose');
-var MongoStore = require('connect-mongo')(session);
-var passport = require('passport');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var session = require("express-session");
+var mongoose = require("mongoose");
+var MongoStore = require("connect-mongo")(session);
+var passport = require("passport");
 
-var conf = require('./constants/conf');
+var conf = require("./constants/conf");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var messagesRouter = require("./routes/messages");
 
 var app = express();
 
@@ -23,38 +24,44 @@ mongoose.connect(conf.mongodburi, {
 });
 
 // load passport configurations
-require('./config/passport');
+require("./config/passport");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({
-  extended: false
-}));
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
 app.use(cookieParser());
 
 // inialize session
-app.use(session({
-  secret: conf.sessionSecretKey,
-  resave: false,
-  saveUninitialized: false,
-  store: new MongoStore({
-    mongooseConnection: mongoose.connection,
-    autoRemove: 'native',
-  }),
-  cookie: {
-    maxAge: 60 * 60 * 1000,
-    secure: true
-  }
-}));
+app.use(
+  session({
+    secret: conf.sessionSecretKey,
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      autoRemove: "native",
+    }),
+    cookie: {
+      maxAge: 60 * 60 * 1000,
+      secure: true,
+    },
+  })
+);
 
 // initialize passport
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/api', indexRouter);
-app.use('/api/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/api", indexRouter);
+app.use("/api/users", usersRouter);
+app.use("/messages", messagesRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -65,13 +72,13 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // only providing error in development
   const message = err.message,
-    error = req.app.get('env') === 'development' ? err : {};
+    error = req.app.get("env") === "development" ? err : {};
 
   // send the error to client
   res.status(err.status || 500);
   res.send({
     message,
-    error
+    error,
   });
 });
 
