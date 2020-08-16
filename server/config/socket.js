eaaa6d493c559e7f6d43464f11c95f60bde4d979
RANDRIANANDRAINA Hajanirina Ridjvan <darijavan@gmail.com>
2020-08-16 12:19:02 +0300

    Update socket.js

--- server/config/socket.js
+++ server/config/socket.js
@@ -1,10 +1,29 @@
+var OptimizedMap = require("../classes/map");
+
 /**
  * Initialize SocketIO server
  * @param {SocketIO.Server} io The socket server
  */
 function initSocket(io) {
-  io.on('connection', function (socket) {
-    // TODO
+  const currentConnections = new OptimizedMap();
+
+  io.on("connection", function (socket) {
+    currentConnections.set(socket, {});
+
+    io.emit("user-connected", currentConnections.size);
+
+    socket.on("user-data", function (data) {
+      currentConnections.set(socket, data);
+    });
+
+    socket.on("send-message", function (data) {
+      if (typeof data.targetId === "string")
+        socket.broadcast.to(data.targetId).emit("message", data.message);
+    });
+
+    socket.on("disconnect", function () {
+      currentConnections.delete(socket);
+    });
   });
 }
 
